5e0cc303f81cf797e67b6bd7e7bb35c18c516c61
@josegarciamanez <jose.garcia.manez@everis.com>
2020-08-01 13:14:16 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+     import { Router, Route, Link } from 'svelte-routing';
+     import Home from './pages/Home.svelte';
+     import About from './pages/About.svelte';
+     export let url = ''; //This property is necessary declare to avoid ignore the Router
+     import Button from './Button.svelte';
+
+     let src =
+          'https://openexpoeurope.com/wp-content/uploads/2019/09/svelte-logo.png';
+</script>
+
+<style lang="scss">
+     img {
+          max-width: 300px;
+     }
+</style>
+
+<svelte:head>
+     <title>Demo - Svelte</title>
+     <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous" />
+</svelte:head>
+
+<Router {url}>
+     <nav>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+     </nav>
+     <div>
+          <Route path="about" component={About} />
+          <!--for now the router just support case sensitive,
+        one workaround colud be add two time the route
+        Example.
+       <Route path="About" component="{About}" /> 
+    -->
+          <Route path="/">
+               <Home />
+          </Route>
+     </div>
+     <div class="text-center">
+          <img {src} alt={src} />
+     </div>
+</Router>
---
+++ src/Button.svelte
@@ -0,0 +1,20 @@
+<script>
+     let counter = 0;
+     const handlerClick = () => {
+          counter++;
+     };
+</script>
+
+<style lang="scss">
+     @import './sass/_variables.scss';
+
+     button {
+          display: block;
+          margin: 50px auto;
+          color: $red;
+     }
+</style>
+
+<button class="btn btn-outline-danger" on:click={handlerClick}>
+     Clicked {counter}
+</button>
